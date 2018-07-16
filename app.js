bce8eab2e44275abb7254d298cfcf316b2e9a51b
autumnFish <517729329@qq.com>
2018-07-16 17:31:47 +0800

    实现验证码判断&用户注册登出功能(不基于数据库)

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ let svgCaptcha = require('svg-captcha');
 let path = require('path');
 // 导入 session模块
 let session = require('express-session');
+// 导入body-parser 格式化表单的数据
+let bodyParser = require('body-parser');
+// 导入mongoDB 
+const MongoClient = require('mongodb').MongoClient;
+// mongoDB 需要使用到的 配置
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'SZHM19';
 
 
 
@@ -24,6 +34,11 @@ app.use(session({
     secret: 'keyboard cat love west blue flower hahahaha'
 }))
 
+// 使用 bodyParser 中间件
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
 
 // 路由--------------------------
 // 路由1
@@ -37,6 +52,38 @@ app.get('/login', (req, res) => {
 })
 
 // 路由2
+// 使用post 提交数据过来 验证用户登陆
+app.post('/login', (req, res) => {
+    // 获取form表单提交的数据
+    // 接收数据
+    // 比较数据
+    let userName = req.body.userName;
+    let userPass = req.body.userPass;
+    // 验证码
+    let code = req.body.code;
+    // 跟 session中的验证码进行比较
+    if (code == req.session.captcha) {
+        // console.log('验证码正确');
+        // 设置session
+        req.session.userInfo = {
+            userName,
+            userPass
+        }
+        // 去首页
+        res.redirect('/index');
+    } else {
+        // console.log('失败');
+        // 打回去
+        // res.redirect('/login');
+        res.setHeader('content-type', 'text/html');
+        res.send('<script>alert("验证码失败");window.location.href="/login"</script>');
+    }
+
+
+    // res.send('login');
+})
+
+// 路由3
 // 生成图片的功能
 // 把这个地址 设置给 登录页的 图片的 src属性
 app.get('/login/captchaImg.png', (req, res) => {
@@ -47,9 +94,82 @@ app.get('/login/captchaImg.png', (req, res) => {
     // 获取session中的值
     // console.log(req.session.info);
     // 保存 验证码的值 到 session 方便后续的使用
-    req.session.captcha = captcha.text;
+    // 为了比较时简单 直接转为小写
+    req.session.captcha = captcha.text.toLocaleLowerCase();
     res.type('svg');
     res.status(200).send(captcha.data);
+})
+
+// 路由4
+// 访问首页 index
+app.get('/index', (req, res) => {
+    // 有session 欢迎
+    if (req.session.userInfo) {
+        // 登陆了
+        res.sendFile(path.join(__dirname, 'static/views/index.html'));
+    } else {
+        // 没有session 去登录页
+        res.setHeader('content-type', 'text/html');
+        res.send("<script>alert('请登录');window.location.href='/login'</script>");
+    }
+})
+
+// 路由5
+// 登出操作
+// 删除 session的值即可
+app.get('/logout', (req, res) => {
+    // 删除session中的 userInfo
+    delete req.session.userInfo;
+
+    // 去登录页即可
+    res.redirect('/login');
+})
+
+// 路由6
+// 展示注册页面
+app.get('/register', (req, res) => {
+    // 直接读取并返回注册页
+    res.sendFile(path.join(__dirname, 'static/views/register.html'));
+})
+
+// 路由7
+app.post('/register', (req, res) => {
+
+    // 获取用户数据
+    let userName = req.body.userName;
+    let userPass = req.body.userPass;
+    console.log(userName);
+    console.log(userPass);
+
+    MongoClient.connect(url,  (err, client)=>{
+        // 连上mongo之后 选择使用的库
+        const db = client.db(dbName);
+        // 选择使用的集合
+        let collection = db.collection('userList');
+
+        // 查询数据
+        collection.find({
+            userName
+        }).toArray((err,doc)=>{
+            console.log(doc);
+            if(doc.length==0){
+                // 没有人
+                // 新增数据
+                collection.insertOne({
+                    userName,
+                    userPass
+                },(err,result)=>{
+                    console.log(err);
+                    // 注册成功了
+                    res.setHeader('content-type','text/html');
+                    res.send("<script>alert('欢迎入坑');window.location='/login'</script>")
+                    // 关闭数据库连接即可
+                    client.close();
+                })
+            }
+        })
+        
+    });
 })
 
 
